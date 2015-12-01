917ed7765dbb2d5f80d11a00d73477177ceb8af2
Tadej <bogataj.tadej@gmail.com>
2015-12-01 22:32:14 +0100

    razred.py

---
+++ razred.py
@@ -0,0 +1,7 @@
+class oznaka:
+
+    def __init__(self, ime, stevilkaClanka):
+        self.ime = ime
+        self.steviloPojavitev = 1
+        self.pojavitevVclanku = []
+        self.pojavitevVclanku.append(stevilkaClanka)
